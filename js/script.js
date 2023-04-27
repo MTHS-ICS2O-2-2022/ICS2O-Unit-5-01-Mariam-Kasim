75622d91faef92c0eb45e1f54d21dbd3a5ee5349
Mariam-Kasim <124389357+Mariam-Kasim@users.noreply.github.com>
2023-04-27 16:45:41 +0000

    commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Mariam Kasim All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mariam Kasim
+// Created on: Apr 26
 // This file contains the JS functions for index.html
+
+"use strict"
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+function myButtonClicked() {
+  //input
+  const numberGuessed = parseInt(document.getElementById("numberGuessed").value)
+
+  //process
+  if (numberGuessed == randomNumber) {
+    //output
+    document.getElementById("answer").innerHTML = "You guessed correctly!"
+  }
+  if (numberGuessed != randomNumber) {
+    //output
+    document.getElementById("answer").innerHTML = "You guessed too high!"
+  }
+}
