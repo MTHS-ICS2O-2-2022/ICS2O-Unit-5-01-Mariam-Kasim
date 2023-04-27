d4e924190cd0d4d440db8ae7741359b4da9a4467
Mariam-Kasim <124389357+Mariam-Kasim@users.noreply.github.com>
2023-04-27 17:22:28 +0000

    added go

---
+++ go_app/main.go
@@ -0,0 +1,38 @@
+// Created by: Mariam Kasim
+// Created on: Apr 2023
+//
+// This program generates a random number between 1 and 6 and asks the user to guess the number.
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	//This function is the main function
+	//variables
+	var guess int
+	var answer int
+
+	//input
+	fmt.Println("Enter a number between 1 and 6: ")
+	fmt.Scanln(&guess)
+
+	//process
+	rand.Seed(time.Now().UnixNano())
+	min := 1
+	max := 6
+	answer = rand.Intn(max-min) + min
+
+	//output
+	if guess == answer {
+		fmt.Println("You guessed correctly!")
+	}
+	if guess != answer {
+		fmt.Println("You guessed wrong! The answer was", answer)
+	}
+	fmt.Println("\nDone.")
+}
